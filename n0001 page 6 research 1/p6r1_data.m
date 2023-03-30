31373da2475d78f2d2fad5e86c6139a308adae96
ymj071371 <ymj071371@alumni.tongji.edu.cn>
2023-03-30 23:18:58 +0800

    update code

---
+++ n0001 page 6 research 1/p6r1_data.m
@@ -0,0 +1,5 @@
+carPrice=[22045;24350;26200;27515;24990];
+payFirst=[1000;1500;750;1500;1000];
+payMonth=[500;500;500;500;500];
+rateYear=[0.0595;0.055;0.0625;0.06;0.059];
+cpr=[carPrice-payFirst payMonth rateYear];
